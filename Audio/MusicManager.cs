e8d3b40b94589c77c01274489146510851cb2037
Sarah Herzog <sarah.ann.herzog@gmail.com>
2016-04-17 01:13:33 +0100

    Move MusicManager into BounderFramework, adds setter for music volume

--- Audio/MusicManager.cs
+++ Audio/MusicManager.cs
@@ -1,6 +1,6 @@
 ﻿// ************************************************************************ 
 // File Name:   MusicManager.cs 
-// Purpose:    	
+// Purpose:    	Holds game music references, fades music tracks in and out.
 // Project:		Framework
 // Author:      Sarah Herzog  
 // Copyright: 	2014 Bounder Games
@@ -8,65 +8,91 @@
 
 
 // ************************************************************************ 
-// Imports 
-// ************************************************************************ 
+#region Imports
+// ************************************************************************
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using BounderFramework;
+#endregion
+// ************************************************************************
 
 
-// ************************************************************************ 
-// Attributes 
-// ************************************************************************ 
-
-
-// ************************************************************************ 
-// Class: MusicManager
-// ************************************************************************ 
-public class MusicManager : Singleton<MusicManager> {
-
-
-    // ********************************************************************
-    // Private Data Members 
-    // ********************************************************************
-	[SerializeField]
-	private FadeAudio m_fadeAudio;
-
-
-	// ********************************************************************
-	public static void FadeOut() { instance.m_fadeAudio.FadeOut(); }
-	// ********************************************************************
-
-
-	// ********************************************************************
-	public static void FadeIn() { instance.m_fadeAudio.FadeIn(); }
-	// ********************************************************************
-	
+// ************************************************************************
+namespace BounderFramework 
+{ 
 	
 	// ********************************************************************
-	public static void PlayMusic(string clipResourceLocation) {
-		AudioClip clip = (AudioClip) Resources.Load(clipResourceLocation);
-		PlayMusic(clip);
-	}
+	// Class: MusicManager
 	// ********************************************************************
+	public class MusicManager : Singleton<MusicManager>
+	{
+
+		// ****************************************************************
+		#region Exposed Data Members
+		// ****************************************************************
+		[SerializeField]
+		private FadeAudio m_fadeAudio;
+		[SerializeField]
+		private string m_musicFolder;
+		#endregion
+		// ****************************************************************
 
 
-    // ********************************************************************
-	public static IEnumerator PlayMusic(AudioClip clip) {
-		Debug.Log ("Queuing clip "+clip.name);
-		// fade current audio out if clip does not match, then when faded out, switch clips and fade back in
-		if (instance.m_fadeAudio.audioSource.clip != clip)
+		// ****************************************************************
+		#region Public Methods
+		// ****************************************************************
+
+
+		// ****************************************************************
+		public static void FadeOut() { instance.m_fadeAudio.FadeOut(); }
+		// ****************************************************************
+
+
+		// ****************************************************************
+		public static void FadeIn() { instance.m_fadeAudio.FadeIn(); }
+		// ****************************************************************
+		
+
+		// ****************************************************************
+		public static void PlayMusic(string clipResourceLocation) 
 		{
-			yield return instance.m_fadeAudio.FadeOut();
-
-			Debug.Log ("Playing clip "+clip.name);
-			instance.m_fadeAudio.audioSource.clip = clip;
-			instance.m_fadeAudio.audioSource.Play();
-			instance.m_fadeAudio.FadeIn();
+			AudioClip clip = (AudioClip) Resources.Load(m_musicFolder+"/"+clipResourceLocation);
+			PlayMusic(clip);
 		}
+		// ****************************************************************
+
+
+		// ****************************************************************
+		public static IEnumerator PlayMusic(AudioClip clip) 
+		{
+			Debug.Log ("Queuing clip "+clip.name);
+			// fade current audio out if clip does not match, then when faded out, switch clips and fade back in
+			if (instance.m_fadeAudio.audioSource.clip != clip)
+			{
+				yield return instance.m_fadeAudio.FadeOut();
+
+				Debug.Log ("Playing clip "+clip.name);
+				instance.m_fadeAudio.audioSource.clip = clip;
+				instance.m_fadeAudio.audioSource.Play();
+				instance.m_fadeAudio.FadeIn();
+			}
+		}
+		// ****************************************************************
+
+
+		// ****************************************************************
+		public static void SetVolume(float _volume) 
+		{
+			if (instance.m_fadeAudio.isFullyAudible)
+				instance.m_fadeAudio.audioSource.volume = _volume;
+			instance.m_fadeAudio.maxVolume = _volume;
+		}
+		// ****************************************************************
+
+
+		// ****************************************************************
+		#endregion
+		// ****************************************************************
 	}
-	// ********************************************************************
-
-
 }
