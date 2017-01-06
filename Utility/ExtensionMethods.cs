9cdae91d718a19e1f1fd3f7a484018e177ac6f65
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-01-06 18:45:06 +0000

    Added utility extensions to convert screen or world points to canvas points, and to print out colors as hex strings

--- Utility/ExtensionMethods.cs
+++ Utility/ExtensionMethods.cs
@@ -107,9 +107,84 @@ public static class ExtensionMethods {
 
 
 	// ********************************************************************
+	// Function:	NullOrEmpty()
+	// Purpose:		Check if a string is either null or empty.
+	// ********************************************************************
 	public static bool NullOrEmpty(this string _string)
 	{
 		return String.IsNullOrEmpty(_string);
 	}
+
 	// ********************************************************************
+	// Function:	WorldToCanvasPoint()
+	// Purpose:		Transforms a world point to the canvas space
+	// ********************************************************************
+	public static Vector2 WorldToCanvasPoint(this Canvas _canvas, Camera _camera, Vector3 _worldPoint)
+	{
+		RectTransform canvasRect = _canvas.GetComponent<RectTransform>();
+
+		//Vector position (percentage from 0 to 1) considering camera size.
+		//For example (0,0) is lower left, middle is (0.5,0.5)
+		Vector2 canvasPoint = _camera.WorldToViewportPoint(_worldPoint);
+
+		//Calculate position considering our percentage, using our canvas size
+		//So if canvas size is (1100,500), and percentage is (0.5,0.5), current value will be (550,250)
+		canvasPoint.x *= canvasRect.sizeDelta.x;
+		canvasPoint.y *= canvasRect.sizeDelta.y;
+
+		//The result is ready, but, this result is correct if canvas recttransform pivot is 0,0 - left lower corner.
+		//But in reality its middle (0.5,0.5) by default, so we remove the amount considering cavnas rectransform pivot.
+		//We could multiply with constant 0.5, but we will actually read the value, so if custom rect transform is passed(with custom pivot) , 
+		//returned value will still be correct.
+		canvasPoint.x -= canvasRect.sizeDelta.x * canvasRect.pivot.x;
+		canvasPoint.y -= canvasRect.sizeDelta.y * canvasRect.pivot.y;
+
+		return canvasPoint;
+	}
+
+	// ********************************************************************
+	// Function:	ScreenToCanvasPoint()
+	// Purpose:		Transforms a screen point to the canvas space
+	// ********************************************************************
+	public static Vector2 ScreenToCanvasPoint(this Canvas _canvas, Camera _camera, Vector2 _screenPoint)
+	{
+		RectTransform canvasRect = _canvas.GetComponent<RectTransform>();
+
+		//Vector position (percentage from 0 to 1) considering camera size.
+		//For example (0,0) is lower left, middle is (0.5,0.5)
+		Vector2 canvasPoint = _camera.ScreenToViewportPoint(_screenPoint);
+
+		//Calculate position considering our percentage, using our canvas size
+		//So if canvas size is (1100,500), and percentage is (0.5,0.5), current value will be (550,250)
+		canvasPoint.x *= canvasRect.sizeDelta.x;
+		canvasPoint.y *= canvasRect.sizeDelta.y;
+
+		//The result is ready, but, this result is correct if canvas recttransform pivot is 0,0 - left lower corner.
+		//But in reality its middle (0.5,0.5) by default, so we remove the amount considering canvas rectransform pivot.
+		//We could multiply with constant 0.5, but we will actually read the value, so if custom rect transform is passed(with custom pivot) , 
+		//returned value will still be correct.
+		canvasPoint.x -= canvasRect.sizeDelta.x * canvasRect.pivot.x;
+		canvasPoint.y -= canvasRect.sizeDelta.y * canvasRect.pivot.y;
+
+		return canvasPoint;
+	}
+
+
+	// ********************************************************************
+	// Function:	ToHex()
+	// Purpose:		Outputs a Color as a hex string
+	// ********************************************************************
+	public static string ToHex(this Color _color)
+	{
+		string z = "#";
+		string alpha = "0123456789ABCDEF";
+		for (int i = 0; i < 3; ++i)
+		{
+			int dec = (int)(_color[i] * 255f);
+			string a = ""+alpha[dec / 16];
+			string b = ""+alpha[dec % 16];
+			z += a + b;
+		}
+		return z;
+	}
 }
