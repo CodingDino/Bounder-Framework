8ee2003f4883735084ff05c8e31c3af378401c13
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-03-01 21:53:39 +0000

    Added AnimationEventCallback for scripts to subscribe to animation events as delegates.

---
+++ Graphics/Animation/AnimationEventCallback.cs
@@ -0,0 +1,73 @@
+﻿// ************************************************************************ 
+// File Name:   SendEventOnAnimationEvent.cs 
+// Purpose:    	
+// Project:		
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: SendEventOnAnimationEvent
+// ************************************************************************
+public class AnimationEventCallback : MonoBehaviour 
+{
+
+	// ********************************************************************
+	#region Delegates 
+	// ********************************************************************
+	public delegate void AnimationEvent();
+	public event AnimationEvent OnAnimationEvent;
+	public delegate void AnimationEventString(string _value);
+	public event AnimationEventString OnAnimationEventString;
+	public delegate void AnimationEventInt(int _value);
+	public event AnimationEventInt OnAnimationEventInt;
+	public delegate void AnimationEventFloat(float _value);
+	public event AnimationEventFloat OnAnimationEventFloat;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Private Methods 
+	// ********************************************************************
+	private void Callback () 
+	{
+		if (OnAnimationEvent != null)
+			OnAnimationEvent();
+	}
+	// ********************************************************************
+	private void StringCallback (string _value) 
+	{
+		if (OnAnimationEventString != null)
+			OnAnimationEventString(_value);
+	}
+	// ********************************************************************
+	private void IntCallback (int _value) 
+	{
+		if (OnAnimationEventInt != null)
+			OnAnimationEventInt(_value);
+	}
+	// ********************************************************************
+	private void FloatCallback (float _value) 
+	{
+		if (OnAnimationEventFloat != null)
+			OnAnimationEventFloat(_value);
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
