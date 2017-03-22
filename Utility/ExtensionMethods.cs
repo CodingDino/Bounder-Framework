a431df9a0ba76b1d91dba73b1e087e6436118950
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-03-22 18:55:17 +0000

    Added extension method for enums for bit flag checking, adding, and removing.

--- Utility/ExtensionMethods.cs
+++ Utility/ExtensionMethods.cs
@@ -252,4 +252,28 @@ public static class ExtensionMethods {
 			_self.Add(_toAdd);
 	}
 
+	// ********************************************************************
+	// Enum extensions
+	// ********************************************************************
+	public static bool Contains(this Enum keys, Enum flag)
+	{
+		ulong keysVal = Convert.ToUInt64(keys);
+		ulong flagVal = Convert.ToUInt64(flag);
+
+		return (keysVal & flagVal) == flagVal;
+	}
+	public static ulong AddFlag(this Enum keys, Enum flag)
+	{
+		ulong keysVal = Convert.ToUInt64(keys);
+		ulong flagVal = Convert.ToUInt64(flag);
+
+		return (keysVal | flagVal);
+	}
+	public static ulong RemoveFlag(this Enum keys, Enum flag)
+	{
+		ulong keysVal = Convert.ToUInt64(keys);
+		ulong flagVal = Convert.ToUInt64(flag);
+
+		return (keysVal & (~flagVal));
+	}
 }
