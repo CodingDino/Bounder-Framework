36ec66342b63589b2e87d68db815e63039461171
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-04-06 19:00:32 +0100

    Audio folder cleanup

--- Audio/AudioInfo.cs
+++ Audio/AudioInfo.cs
@@ -19,88 +19,88 @@ using BounderFramework;
 // ************************************************************************
 namespace BounderFramework 
 { 
-	// ******************************************************************** 
-	#region Class: AudioInfo
+// ************************************************************************
+#region Class: AudioInfo
+// ************************************************************************
+[System.Serializable]
+public class AudioInfo : Archive 
+{
+	
 	// ********************************************************************
-	[System.Serializable]
-	public class AudioInfo : Archive 
-	{
-		
-		// ****************************************************************
-		#region Public Data Members
-		// ****************************************************************
-		public AudioCategory category = AudioCategory.EFFECTS;
-		public AudioChannelOverride overrideChannelLimit = AudioChannelOverride.NONE;
-		public float volume = 1.0f;
-		public float volumeFuzz = 0.0f;
-		public float pitch = 1.0f;
-		public float pitchFuzz = 0.0f;
-		public bool loop = false;
-		public float fadeDuration = 0.0f;
-		#endregion
-		// ****************************************************************
-
-
-		// ****************************************************************
-		#region Archive Methods
-		// ****************************************************************
-		public virtual bool Load(JSON _JSON)
-		{
-			bool success = true;
-
-			success &= _JSON["category"].GetEnum<AudioCategory>(ref category);
-			success &= _JSON["override"].GetEnum<AudioChannelOverride>(ref overrideChannelLimit);
-			success &= _JSON["volume"].Get(ref volume);
-			success &= _JSON["volumeFuzz"].Get(ref volumeFuzz);
-			success &= _JSON["pitch"].Get(ref pitch);
-			success &= _JSON["pitchFuzz"].Get(ref pitchFuzz);
-			success &= _JSON["loop"].Get(ref loop);
-			success &= _JSON["fadeDuration"].Get(ref fadeDuration);
-
-			return success;
-		}
-		// ****************************************************************
-		public virtual JSON Save()
-		{
-			JSON save = new JSON();
-
-			save["category"].data = category.ToString();
-			save["override"].data = overrideChannelLimit.ToString();
-			save["volume"].data = volume;
-			save["volumeFuzz"].data = volumeFuzz;
-			save["pitch"].data = pitch;
-			save["pitchFuzz"].data = pitchFuzz;
-			save["shouldLoop"].data = loop;
-			save["fadeDuration"].data = fadeDuration;
-
-			return save;
-		}
-		// ****************************************************************
-		#endregion
-		// ****************************************************************
-
-
-		// ****************************************************************
-		#region Public Methods
-		// ****************************************************************
-		public AudioInfo() { }
-		// ****************************************************************
-		public AudioInfo(AudioCategory _category) 
-		{ 
-			category = _category;
-			if (_category == AudioCategory.MUSIC)
-			{
-				overrideChannelLimit = AudioChannelOverride.REPLACE;
-				fadeDuration = 1.0f;
-				loop = true;
-			}
-		}
-		// ****************************************************************
-		#endregion
-		// ****************************************************************
-
-	}
+	#region Public Data Members
+	// ********************************************************************
+	public AudioCategory category = AudioCategory.EFFECTS;
+	public AudioChannelOverride overrideChannelLimit = AudioChannelOverride.NONE;
+	public float volume = 1.0f;
+	public float volumeFuzz = 0.0f;
+	public float pitch = 1.0f;
+	public float pitchFuzz = 0.0f;
+	public bool loop = false;
+	public float fadeDuration = 0.0f;
 	#endregion
 	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Archive Methods
+	// ********************************************************************
+	public virtual bool Load(JSON _JSON)
+	{
+		bool success = true;
+
+		success &= _JSON["category"].GetEnum<AudioCategory>(ref category);
+		success &= _JSON["override"].GetEnum<AudioChannelOverride>(ref overrideChannelLimit);
+		success &= _JSON["volume"].Get(ref volume);
+		success &= _JSON["volumeFuzz"].Get(ref volumeFuzz);
+		success &= _JSON["pitch"].Get(ref pitch);
+		success &= _JSON["pitchFuzz"].Get(ref pitchFuzz);
+		success &= _JSON["loop"].Get(ref loop);
+		success &= _JSON["fadeDuration"].Get(ref fadeDuration);
+
+		return success;
+	}
+	// ********************************************************************
+	public virtual JSON Save()
+	{
+		JSON save = new JSON();
+
+		save["category"].data = category.ToString();
+		save["override"].data = overrideChannelLimit.ToString();
+		save["volume"].data = volume;
+		save["volumeFuzz"].data = volumeFuzz;
+		save["pitch"].data = pitch;
+		save["pitchFuzz"].data = pitchFuzz;
+		save["shouldLoop"].data = loop;
+		save["fadeDuration"].data = fadeDuration;
+
+		return save;
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Public Methods
+	// ********************************************************************
+	public AudioInfo() { }
+	// ********************************************************************
+	public AudioInfo(AudioCategory _category) 
+	{ 
+		category = _category;
+		if (_category == AudioCategory.MUSIC)
+		{
+			overrideChannelLimit = AudioChannelOverride.REPLACE;
+			fadeDuration = 1.0f;
+			loop = true;
+		}
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
 }
 // ************************************************************************
--- Audio/AudioManager.cs
+++ Audio/AudioManager.cs
@@ -22,211 +22,211 @@ using BounderFramework;
 // ************************************************************************
 namespace BounderFramework 
 { 
+// ************************************************************************
+#region Enum: AudioCategory
+// ************************************************************************
+public enum AudioCategory
+{
+	INVALID = -1,
+	// ---
+	Master = 0, // NOTE: Must be camel case for Unity interaction
+	EFFECTS,
+	MUSIC,
+	DIALOGUE,
+	// ---
+	NUM
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************
+#region Enum: AudioChannelOverride
+// ************************************************************************
+public enum AudioChannelOverride
+{
+	INVALID = -1,
+	// ---
+	NONE = 0,
+	ADD,
+	REPLACE,
+	// ---
+	NUM
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************
+#region Class: AudioCategorySettings
+// ************************************************************************
+[System.Serializable]
+public class AudioCategorySettings
+{
+	public AudioCategory category;
+	public AudioMixerGroup group;
+	public int numChannels;
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************
+#region Class: AudioManager
+// ************************************************************************
+public class AudioManager : Singleton<AudioManager>
+{
+
 	// ********************************************************************
-	#region Enum: AudioCategory
+	#region Exposed Data Members
 	// ********************************************************************
-	public enum AudioCategory
-	{
-		INVALID = -1,
-		// ---
-		Master = 0, // NOTE: Must be camel case for Unity interaction
-		EFFECTS,
-		MUSIC,
-		DIALOGUE,
-		// ---
-		NUM
-	}
+	[SerializeField]
+	private string m_audioFolder;
+	[SerializeField]
+	private GameObject m_audioObjectPrefab;
+	[SerializeField]
+	private AudioCategorySettings[] m_audioCategorySettings;
 	#endregion
 	// ********************************************************************
 
 
 	// ********************************************************************
-	#region Enum: AudioChannelOverride
+	#region Private Data Members
 	// ********************************************************************
-	public enum AudioChannelOverride
-	{
-		INVALID = -1,
-		// ---
-		NONE = 0,
-		ADD,
-		REPLACE,
-		// ---
-		NUM
-	}
+	private Dictionary<AudioCategory,AudioCategorySettings> m_audioCategorySettings_Internal = new Dictionary<AudioCategory,AudioCategorySettings>();
+	private Dictionary<AudioCategory,ObjectPool> m_audioObjects = new Dictionary<AudioCategory,ObjectPool>();
 	#endregion
 	// ********************************************************************
 
 
 	// ********************************************************************
-	#region Class: AudioCategorySettings
+	#region MonoBehaviour Methods
 	// ********************************************************************
-	[System.Serializable]
-	public class AudioCategorySettings
+	void Awake()
 	{
-		public AudioCategory category;
-		public AudioMixerGroup group;
-		public int numChannels;
+		for (int i = 0; i < m_audioCategorySettings.Length; ++i)
+		{
+			m_audioCategorySettings_Internal[m_audioCategorySettings[i].category] = m_audioCategorySettings[i];
+			m_audioObjects[m_audioCategorySettings[i].category] = new ObjectPool(m_audioObjectPrefab);
+		}
 	}
+	// ********************************************************************
 	#endregion
 	// ********************************************************************
 
 
 	// ********************************************************************
-	#region Class: AudioManager
+	#region Public Methods
 	// ********************************************************************
-	public class AudioManager : Singleton<AudioManager>
+	public static AudioClip LoadClip(string _track, AudioInfo _info)
 	{
+		string filepath = instance.m_audioFolder+_info.category.ToString()+"/"+_track;
+		AudioClip clip = Resources.Load<AudioClip>(filepath);
+		if (clip == null)
+			Debug.LogError("AudioManager.LoadClip failed for "+filepath);
+		return clip;
+	}
+	// ********************************************************************
+	public static AudioCategorySettings GetAudioSettings(AudioCategory _category) 
+	{ 
+		return instance.m_audioCategorySettings_Internal[_category];
+	}
+	// ********************************************************************
+	public static AudioObject Play(string _track, 
+                                   AudioCategory _category)
+	{
+		return Play(_track, new AudioInfo(_category));
+	}
+	// ********************************************************************
+	public static AudioObject Play(string _track, 
+                                   AudioInfo _info)
+	{
+		if (_info.overrideChannelLimit == AudioChannelOverride.NONE && !ChannelAvailable(_info.category))
+			return null;
 
-		// ****************************************************************
-		#region Exposed Data Members
-		// ****************************************************************
-		[SerializeField]
-		private string m_audioFolder;
-		[SerializeField]
-		private GameObject m_audioObjectPrefab;
-		[SerializeField]
-		private AudioCategorySettings[] m_audioCategorySettings;
-		#endregion
-		// ****************************************************************
+		string filepath = instance.m_audioFolder+_info.category.ToString()+"/"+_track;
+		AudioClip clip = Resources.Load<AudioClip>(filepath);
 
-
-		// ****************************************************************
-		#region Private Data Members
-		// ****************************************************************
-		private Dictionary<AudioCategory,AudioCategorySettings> m_audioCategorySettings_Internal = new Dictionary<AudioCategory,AudioCategorySettings>();
-		private Dictionary<AudioCategory,ObjectPool> m_audioObjects = new Dictionary<AudioCategory,ObjectPool>();
-		#endregion
-		// ****************************************************************
-
-
-		// ****************************************************************
-		#region MonoBehaviour Methods
-		// ****************************************************************
-		void Awake()
+		if (clip == null)
 		{
-			for (int i = 0; i < m_audioCategorySettings.Length; ++i)
-			{
-				m_audioCategorySettings_Internal[m_audioCategorySettings[i].category] = m_audioCategorySettings[i];
-				m_audioObjects[m_audioCategorySettings[i].category] = new ObjectPool(m_audioObjectPrefab);
-			}
+			Debug.LogError("Attempt to load non-existant audio file: "+filepath);
+			return null;
 		}
-		// ****************************************************************
-		#endregion
-		// ****************************************************************
 
+		return Play(clip,_info);
+	}
+	// ********************************************************************
+	public static AudioObject Play(AudioClip _clip, 
+                                   AudioCategory _category)
+	{
+		return Play(_clip, new AudioInfo(_category));
+	}
+	// ********************************************************************
+	public static AudioObject Play(AudioClip _clip, 
+                                   AudioInfo _info)
+	{
+		if (_info.overrideChannelLimit == AudioChannelOverride.NONE && !ChannelAvailable(_info.category))
+			return null;
 
-		// ****************************************************************
-		#region Public Methods
-		// ****************************************************************
-		public static AudioClip LoadClip(string _track, AudioInfo _info)
+		bool replacing = !ChannelAvailable(_info.category) && _info.overrideChannelLimit == AudioChannelOverride.REPLACE;
+		if (replacing)
 		{
-			string filepath = instance.m_audioFolder+_info.category.ToString()+"/"+_track;
-			AudioClip clip = Resources.Load<AudioClip>(filepath);
-			if (clip == null)
-				Debug.LogError("AudioManager.LoadClip failed for "+filepath);
-			return clip;
-		}
-		// ****************************************************************
-		public static AudioCategorySettings GetAudioSettings(AudioCategory _category) 
-		{ 
-			return instance.m_audioCategorySettings_Internal[_category];
-		}
-		// ****************************************************************
-		public static AudioObject Play(string _track, 
-	                                   AudioCategory _category)
-		{
-			return Play(_track, new AudioInfo(_category));
-		}
-		// ****************************************************************
-		public static AudioObject Play(string _track, 
-	                                   AudioInfo _info)
-		{
-			if (_info.overrideChannelLimit == AudioChannelOverride.NONE && !ChannelAvailable(_info.category))
-				return null;
-
-			string filepath = instance.m_audioFolder+_info.category.ToString()+"/"+_track;
-			AudioClip clip = Resources.Load<AudioClip>(filepath);
-
-			if (clip == null)
-			{
-				Debug.LogError("Attempt to load non-existant audio file: "+filepath);
-				return null;
-			}
-
-			return Play(clip,_info);
-		}
-		// ****************************************************************
-		public static AudioObject Play(AudioClip _clip, 
-	                                   AudioCategory _category)
-		{
-			return Play(_clip, new AudioInfo(_category));
-		}
-		// ****************************************************************
-		public static AudioObject Play(AudioClip _clip, 
-	                                   AudioInfo _info)
-		{
-			if (_info.overrideChannelLimit == AudioChannelOverride.NONE && !ChannelAvailable(_info.category))
-				return null;
-
-			bool replacing = !ChannelAvailable(_info.category) && _info.overrideChannelLimit == AudioChannelOverride.REPLACE;
-			if (replacing)
-			{
-				AudioObject oldObject = instance.m_audioObjects[_info.category].FirstActive.GetComponent<AudioObject>();
-				
-				// If we're trying to replace it with the same thing, don't.
-				if (oldObject.audioClip.ToString() == _clip.ToString())
-					return null;
+			AudioObject oldObject = instance.m_audioObjects[_info.category].FirstActive.GetComponent<AudioObject>();
 			
-				oldObject.Fade(false);
-			}
-
-			GameObject audioGameObject = instance.m_audioObjects[_info.category].RequestObject();
-			audioGameObject.transform.SetParent(instance.transform);
-			AudioObject audioObject = audioGameObject.GetComponent<AudioObject>();
-			audioObject.audioInfo = _info;
-			audioObject.audioClip = _clip;
-
-			audioObject.Apply();
-			audioObject.audioSource.Play();
-
-			if (_info.fadeDuration != 0)
-			{
-				audioObject.audioSource.volume=0;
-				audioObject.Fade(true);
-			}
-
-			return audioObject;
+			// If we're trying to replace it with the same thing, don't.
+			if (oldObject.audioClip.ToString() == _clip.ToString())
+				return null;
+		
+			oldObject.Fade(false);
 		}
-		// ****************************************************************
-		public static bool ChannelAvailable(AudioCategory _category)
+
+		GameObject audioGameObject = instance.m_audioObjects[_info.category].RequestObject();
+		audioGameObject.transform.SetParent(instance.transform);
+		AudioObject audioObject = audioGameObject.GetComponent<AudioObject>();
+		audioObject.audioInfo = _info;
+		audioObject.audioClip = _clip;
+
+		audioObject.Apply();
+		audioObject.audioSource.Play();
+
+		if (_info.fadeDuration != 0)
 		{
-			return instance.m_audioObjects[_category].Count < instance.m_audioCategorySettings_Internal[_category].numChannels;
+			audioObject.audioSource.volume=0;
+			audioObject.Fade(true);
 		}
-		// ****************************************************************
-		public static List<AudioObject> GetActiveAudioForCategory(AudioCategory _category)
-		{
-			List<AudioObject> audioObjects = new List<AudioObject>();
-			List<ObjectPoolObject> objectPoolObjects = instance.m_audioObjects[_category].activeObjects;
-			for (int i = 0; i < objectPoolObjects.Count; ++i)
-			{
-				audioObjects.Add(objectPoolObjects[i].GetComponent<AudioObject>());
-			}
-			return audioObjects;
-		}
-		// ****************************************************************
-		public static void FadeOutCategory(AudioCategory _category)
-		{
-			List<AudioObject> audioObjects = AudioManager.GetActiveAudioForCategory(AudioCategory.MUSIC);
-			for (int i = 0; i < audioObjects.Count; ++i)
-			{
-				audioObjects[i].Fade(false);
-			}
-		}
-		// ****************************************************************
-		#endregion
-		// ****************************************************************
+
+		return audioObject;
 	}
+	// ********************************************************************
+	public static bool ChannelAvailable(AudioCategory _category)
+	{
+		return instance.m_audioObjects[_category].Count < instance.m_audioCategorySettings_Internal[_category].numChannels;
+	}
+	// ********************************************************************
+	public static List<AudioObject> GetActiveAudioForCategory(AudioCategory _category)
+	{
+		List<AudioObject> audioObjects = new List<AudioObject>();
+		List<ObjectPoolObject> objectPoolObjects = instance.m_audioObjects[_category].activeObjects;
+		for (int i = 0; i < objectPoolObjects.Count; ++i)
+		{
+			audioObjects.Add(objectPoolObjects[i].GetComponent<AudioObject>());
+		}
+		return audioObjects;
+	}
+	// ********************************************************************
+	public static void FadeOutCategory(AudioCategory _category)
+	{
+		List<AudioObject> audioObjects = AudioManager.GetActiveAudioForCategory(AudioCategory.MUSIC);
+		for (int i = 0; i < audioObjects.Count; ++i)
+		{
+			audioObjects[i].Fade(false);
+		}
+	}
+	// ********************************************************************
 	#endregion
 	// ********************************************************************
+}
+#endregion
+// ************************************************************************
 }
 // ************************************************************************
