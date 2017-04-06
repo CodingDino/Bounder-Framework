bb860622c71ae50d1926aa356e26d2dc40cb08bf
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-04-06 09:29:45 +0100

    Remove re-scaling of vector to original magnitude in Randomise extension - this was causing zero length vectors to fail randomisation

--- ExtensionMethods/VectorExtension.cs
+++ ExtensionMethods/VectorExtension.cs
@@ -27,29 +27,21 @@ public static class VectorExtension
 	// ********************************************************************
 	public static Vector2 Randomise(this Vector2 _this)
 	{
-		float magnitude = _this.magnitude;
-
 		_this.x = Random.Range(-1.0f,1.0f);
 		_this.y = Random.Range(-1.0f,1.0f);
 
 		_this.Normalize();
-
-		_this *= magnitude;
 
 		return _this;
 	}
 	// ********************************************************************
 	public static Vector3 Randomise(this Vector3 _this)
 	{
-		float magnitude = _this.magnitude;
-
 		_this.x = Random.Range(-1.0f,1.0f);
 		_this.y = Random.Range(-1.0f,1.0f);
 		_this.z = Random.Range(-1.0f,1.0f);
 
 		_this.Normalize();
-
-		_this *= magnitude;
 
 		return _this;
 	}
