2feeccf1bbd489119f9cba8d5b379c53b314244c
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-04-07 15:30:06 +0100

    Added color helper method to convert hex string to color, improved method for converting color to hex string

--- ExtensionMethods/ColorExtension.cs
+++ ExtensionMethods/ColorExtension.cs
@@ -25,16 +25,46 @@ public static class ColorExtension
 	// ********************************************************************
 	public static string ToHex(this Color _color)
 	{
-		string z = "#";
-		string alpha = "0123456789ABCDEF";
-		for (int i = 0; i < 3; ++i)
+		Color32 byteColor = _color;
+		return byteColor.ToHex();
+	}
+	// ********************************************************************
+	public static string ToHex(this Color32 _color)
+	{
+		return "#" + _color.r.ToString("X2") + _color.g.ToString("X2") + _color.b.ToString("X2");
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Static Methods 
+	// ********************************************************************
+	public static Color CreateFromHex(string _hex)
+	{
+		if (_hex.NullOrEmpty())
 		{
-			int dec = (int)(_color[i] * 255f);
-			string a = ""+alpha[dec / 16];
-			string b = ""+alpha[dec % 16];
-			z += a + b;
+			Debug.LogError("CreateFromHex failed - string empty");
+			return Color.clear;
 		}
-		return z;
+		_hex = _hex.Replace("#","");
+		_hex = _hex.Replace("0x","");
+		if (_hex.Length < 6)
+		{
+			Debug.LogError("CreateFromHex failed - too short string "+_hex);
+			return Color.clear;
+		}
+
+		_hex = _hex.ToUpper();
+		string alpha = "0123456789ABCDEF";
+		Color color = Color.black;
+		for (int i = 0; i < 4 && i*2 < _hex.Length; ++i)
+		{
+			color[i] = ((float)byte.Parse(_hex.Substring(i*2,2), System.Globalization.NumberStyles.HexNumber))/255.0f;
+		}
+
+		return color;
 	}
 	// ********************************************************************
 	#endregion
