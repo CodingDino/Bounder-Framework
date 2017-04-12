790280ea233eeb29f57be59b81b4033e48a69eae
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-04-13 00:43:31 +0100

    TypeExtension - added function to get list of all subtypes for given type.

---
+++ ExtensionMethods/TypeExtension.cs
@@ -0,0 +1,43 @@
+﻿// ************************************************************************ 
+// File Name:   TypeExtension.cs 
+// Purpose:    	Extends the Type class
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: TypeExtension
+// ************************************************************************
+public static class TypeExtension 
+{
+
+	// ********************************************************************
+	#region Extension Methods 
+	// ********************************************************************
+	public static Type[] GetSubTypes(this Type _type)
+	{
+		Type[] allTypes = System.Reflection.Assembly.GetExecutingAssembly().GetTypes();
+		Type[] validTypes = (from System.Type type in allTypes where type.IsSubclassOf(_type) select type).ToArray();
+		return validTypes;
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
