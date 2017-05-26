d60c7d7c5881bd7ffe9f610140c80979a799cc0e
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-05-26 14:53:18 +0100

    Added dropdown selector for the LayerSelector to avoid typos

--- Graphics/LayerSelecter.cs
+++ Graphics/LayerSelecter.cs
@@ -13,6 +13,12 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+using System.Reflection;
+#endif
 
 
 // ************************************************************************ 
@@ -68,4 +74,56 @@ public class LayerSelecter : MonoBehaviour {
 			GetComponent<Renderer>().sortingOrder = m_orderInLayer;
 		}
 	}
+
+	// ********************************************************************
+	#if UNITY_EDITOR
+	// ********************************************************************
+	[CustomEditor(typeof(LayerSelecter))]
+	public class Inspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			LayerSelecter layerSelecter = (LayerSelecter)target;
+			SerializedObject serialized = new SerializedObject(target);
+			serialized.Update();
+
+			// default - just draw objects
+			SerializedProperty property = serialized.GetIterator();
+			property.NextVisible(true);
+			do
+			{
+				// Dropdown list for graphic layers
+				if (property.name == "m_sortingLayer")
+				{
+					Type utilityType = typeof(InternalEditorUtility);
+					PropertyInfo sortingLayersProperty = utilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+					string[] sortingLayers = (string[])sortingLayersProperty.GetValue(null,new object[0]);
+					// find current layer
+					int currentLayer = sortingLayers.Length;
+					for (int i = 0; i < sortingLayers.Length; ++i)
+					{
+						if (sortingLayers[i] == layerSelecter.m_sortingLayer)
+						{
+							currentLayer = i;
+						}
+					}
+					int newChoice = EditorGUILayout.Popup("Layer",currentLayer, sortingLayers);
+					if (newChoice != currentLayer)
+					{
+						layerSelecter.m_sortingLayer = sortingLayers[newChoice];
+						EditorUtility.SetDirty(target);
+					}
+				}
+				else
+				{
+					EditorGUILayout.PropertyField(property, !property.hasVisibleChildren || property.isExpanded);
+				}
+			} while (property.NextVisible(false));
+
+			serialized.ApplyModifiedProperties();
+		}
+	}
+	// ********************************************************************
+	#endif
+	// ********************************************************************
 }
