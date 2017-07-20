057e454e5659a82b93d596a7dfc6c5739db7d358
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-07-20 10:33:51 +0100

    Added SortingLayerProperty, implemented in LayerSelecter

--- Graphics/LayerSelecter.cs
+++ Graphics/LayerSelecter.cs
@@ -8,7 +8,7 @@
 
 
 // ************************************************************************ 
-// Imports 
+#region Imports 
 // ************************************************************************ 
 using UnityEngine;
 using System.Collections;
@@ -19,38 +19,30 @@ using UnityEditorInternal;
 using System;
 using System.Reflection;
 #endif
+#endregion
+// ************************************************************************
 
 
 // ************************************************************************ 
-// Attributes 
+#region Class: LayerSelecter
 // ************************************************************************ 
 [ExecuteInEditMode]
-
-
-// ************************************************************************ 
-// Class: LayerSelecter
-// ************************************************************************ 
-public class LayerSelecter : MonoBehaviour {
-
-
+public class LayerSelecter : MonoBehaviour 
+{
     // ********************************************************************
-    // Private Data Members 
+	#region Private Data Members 
     // ********************************************************************
 	[SerializeField]
+	[SortingLayerAttribute]
 	private string m_sortingLayer = "Default";
 	[SerializeField]
 	private int m_orderInLayer = 0;
-	 
+	#endregion
+	// ********************************************************************
 	
 	
     // ********************************************************************
-    // Properties 
-    // ********************************************************************
-	
-	
-    // ********************************************************************
-    // Function:	Start()
-	// Purpose:		Run when new instance of the object is created.
+	#region MonoBehaviour Methods
     // ********************************************************************
 	void Start () {
 		if (GetComponent<Renderer>() != null)
@@ -61,11 +53,6 @@ public class LayerSelecter : MonoBehaviour {
 		if (Application.isPlaying)
 			this.enabled = false;
 	}
-	
-	
-    // ********************************************************************
-    // Function:	Update()
-	// Purpose:		Called once per frame.
     // ********************************************************************
 	void Update () {
 		if (GetComponent<Renderer>() != null)
@@ -74,64 +61,10 @@ public class LayerSelecter : MonoBehaviour {
 			GetComponent<Renderer>().sortingOrder = m_orderInLayer;
 		}
 	}
-
-
 	// ********************************************************************
-	#if UNITY_EDITOR
-	// ********************************************************************
-	public static void DrawSortingGUI(UnityEngine.Object _target, string _paramName, ref string _currentLayer)
-	{
-		SerializedObject serialized = new SerializedObject(_target);
-		serialized.Update();
-
-		// default - just draw objects
-		SerializedProperty property = serialized.GetIterator();
-		property.NextVisible(true);
-		do
-		{
-			// Dropdown list for graphic layers
-			if (property.name == _paramName)
-			{
-				Type utilityType = typeof(InternalEditorUtility);
-				PropertyInfo sortingLayersProperty = utilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
-				string[] sortingLayers = (string[])sortingLayersProperty.GetValue(null,new object[0]);
-				// find current layer
-				int currentLayer = sortingLayers.Length;
-				for (int i = 0; i < sortingLayers.Length; ++i)
-				{
-					if (sortingLayers[i] == _currentLayer)
-					{
-						currentLayer = i;
-					}
-				}
-				int newChoice = EditorGUILayout.Popup("Layer",currentLayer, sortingLayers);
-				if (newChoice != currentLayer)
-				{
-					_currentLayer = sortingLayers[newChoice];
-					EditorUtility.SetDirty(_target);
-				}
-			}
-			else
-			{
-				EditorGUILayout.PropertyField(property, !property.hasVisibleChildren || property.isExpanded);
-			}
-		} while (property.NextVisible(false));
-
-		serialized.ApplyModifiedProperties();
-	}
-
-
-
-	[CustomEditor(typeof(LayerSelecter))]
-	public class Inspector : Editor
-	{
-		public override void OnInspectorGUI()
-		{
-			LayerSelecter layerSelecter = (LayerSelecter)target;
-			DrawSortingGUI(target,"m_sortingLayer",ref layerSelecter.m_sortingLayer);
-		}
-	}
-	// ********************************************************************
-	#endif
+	#endregion
 	// ********************************************************************
 }
+// ************************************************************************
+#endregion
+// ************************************************************************
---
+++ Graphics/SortingLayerProperty.cs
@@ -0,0 +1,75 @@
+﻿// ************************************************************************ 
+// File Name:   SortingLayerProperyDrawer.cs 
+// Purpose:    	Property drawer for showing a sorting layer
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2014 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+using System.Reflection;
+#endif
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: SortingLayerProperyDrawer
+// ************************************************************************ 
+public class SortingLayerAttribute : PropertyAttribute {}
+// ************************************************************************
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: SortingLayerProperyDrawer
+// ************************************************************************ 
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(SortingLayerAttribute))]
+public class SortingLayerProperyDrawer : PropertyDrawer 
+{
+	private int textHeight = 16;
+
+	public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+	{
+		EditorGUI.BeginProperty( _position, _label, _property);
+		int origIndent = EditorGUI.indentLevel;
+		Type utilityType = typeof(InternalEditorUtility);
+		PropertyInfo sortingLayersProperty = utilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+		string[] values = (string[])sortingLayersProperty.GetValue(null,new object[0]);
+		int index = 0;
+		for(; index < values.Length -1; ++index )
+		{
+			if (values[index].Equals(_property.stringValue))
+				break;
+		}
+
+		index = EditorGUI.Popup( new Rect (_position.xMin,
+		                                   _position.yMin, 
+		                                   _position.width,
+		                                   textHeight),
+		                        _property.displayName,
+		                        index,
+		                        values);
+
+		if (index < values.Length)
+			_property.stringValue = values[index];
+
+		EditorGUI.indentLevel = origIndent;
+		EditorGUI.EndProperty();
+		
+	}
+}
+// ************************************************************************
+#endif
+#endregion
+// ************************************************************************
