8da2c9bf1ff21b2ef358333e72160d0a238a45dd
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-07-29 00:54:36 +0100

    UI Buttons utility scripts added to framework

---
+++ UI/Buttons/ButtonBroadcast.cs
@@ -0,0 +1,53 @@
+﻿// ************************************************************************ 
+// File Name:   ButtonBroadcast.cs 
+// Purpose:    	Sends out button press event
+// Project:		Armoured Engines
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BounderFramework;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ButtonPressEvent
+// ************************************************************************
+public class ButtonPressEvent : GameEvent 
+{
+	public string buttonName;
+	public ButtonPressEvent(string _buttonName)
+	{
+		buttonName = _buttonName;
+	}
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ButtonBroadcast
+// ************************************************************************
+public class ButtonBroadcast : MonoBehaviour 
+{
+	// ********************************************************************
+	#region Public Methods 
+	// ********************************************************************
+	public void ButtonPressed()
+	{
+		Events.Raise( new ButtonPressEvent(name) );
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+}
+#endregion
+// ************************************************************************
---
+++ UI/Buttons/ButtonClosePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClosePanel : MonoBehaviour {
+
+
+	// ********************************************************************
+	#region Public Methods 
+	// ********************************************************************
+	public void ClosePanel (string _id) 
+	{
+		PanelManager.ClosePanel(_id);
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+	
+}
