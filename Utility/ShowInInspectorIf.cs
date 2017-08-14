7abc5b77604b48df3f9aa14dcafc780f78f34581
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-08-14 20:20:32 +0100

    ShowInInspectorIf - property attribute used to conditionally hide variables based on boolean variables in the same script.

---
+++ Utility/ShowInInspectorIf.cs
@@ -0,0 +1,103 @@
+﻿// ************************************************************************ 
+// File Name:   ShowInInspectorIf.cs 
+// Purpose:    	Property drawer for conditionally hiding in the inspector
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ShowInInspectorIf
+// ************************************************************************ 
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class ShowInInspectorIf : PropertyAttribute 
+{
+	public string conditionalSourceField = "";
+	public bool compare = true;
+
+	public ShowInInspectorIf(string _conditionalSourceField)
+	{
+		conditionalSourceField = _conditionalSourceField;
+		compare = true;
+	}
+
+	public ShowInInspectorIf(string _conditionalSourceField, bool _compare)
+	{
+		conditionalSourceField = _conditionalSourceField;
+		compare = _compare;
+	}
+}
+// ************************************************************************
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ShowInInspectorIfProperyDrawer
+// ************************************************************************ 
+[CustomPropertyDrawer(typeof(ShowInInspectorIf))]
+public class ShowInInspectorIfProperyDrawer : PropertyDrawer 
+{
+	public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+	{
+		ShowInInspectorIf showInInspector = (ShowInInspectorIf)attribute;
+		bool enabled = ShouldShow(showInInspector, _property);
+		bool wasEnabled = GUI.enabled;
+		GUI.enabled = enabled;
+		if (enabled)
+		{
+			EditorGUI.PropertyField(_position, _property, _label, true);
+		}
+
+		GUI.enabled = wasEnabled;
+	}
+
+	private bool ShouldShow(ShowInInspectorIf _attribute, SerializedProperty _property)
+	{
+		bool enabled = true;
+		string propertyPath = _property.propertyPath;
+		string conditionPath = propertyPath.Replace(_property.name, _attribute.conditionalSourceField);
+		SerializedProperty sourcePropertyValue = _property.serializedObject.FindProperty(conditionPath);
+
+		if (sourcePropertyValue != null)
+		{
+			enabled = sourcePropertyValue.boolValue == _attribute.compare;
+		}
+		else
+		{
+			Debug.LogWarning("Attempting to use ShowInInspectorIf but no matching SourcePropertyValue found in object: "+_attribute.conditionalSourceField);
+		}
+
+		return enabled;
+	}
+
+	public override float GetPropertyHeight(SerializedProperty _property, GUIContent _label)
+	{
+		ShowInInspectorIf showInInspector = (ShowInInspectorIf)attribute;
+		bool enabled = ShouldShow(showInInspector, _property);
+
+		if (enabled)
+		{
+			return EditorGUI.GetPropertyHeight(_property, _label);
+		}
+		else
+		{
+			return -EditorGUIUtility.standardVerticalSpacing;
+		}
+	}
+}
+// ************************************************************************
+#endregion
+// ************************************************************************
