135b02e014f7f0284e0d3c1645e3cc185e4c5bd2
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-09-04 14:32:37 +0100

    MoveToTarget - added isMoving property exposing m_moving variable

--- Utility/MoveToTarget.cs
+++ Utility/MoveToTarget.cs
@@ -53,6 +53,7 @@ public class MoveToTarget : MonoBehaviour {
 	// ********************************************************************
 	public Vector3 targetPoint { get { return m_targetPoint; } }
 	public bool hasTarget { get { return m_moving; } }
+	public bool isMoving { get { return m_moving; } }
 	public float duration { set { m_duration = value; }}
 	public float speed { set { m_speed = value; }}
 
