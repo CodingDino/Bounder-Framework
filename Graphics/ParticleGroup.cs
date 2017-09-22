324596d391c0de95b7c15c856fabdac5980213e3
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-09-22 22:09:43 +0100

    ParticleGroup - A script for packaging a group of particle systems for use by other scripts

---
+++ Graphics/ParticleGroup.cs
@@ -0,0 +1,54 @@
+﻿// ************************************************************************ 
+// File Name:   ParticleGroup.cs 
+// Purpose:    	Specify a group of particles for various purposes
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************
+namespace BounderFramework { 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BounderFramework;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ParticleGroup
+// ************************************************************************ 
+public class ParticleGroup : MonoBehaviour 
+{
+	// ********************************************************************
+	#region Exposed Data Members 
+	// ********************************************************************
+	[SerializeField]
+	[Tooltip("ID for this")]
+	private string m_id = "";
+	[SerializeField]
+	[Tooltip("List of particles")]
+	private List<ParticleSystem> m_particles = new List<ParticleSystem>();
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Properties 
+	// ********************************************************************
+	public string id { get { return m_id; } }
+	public List<ParticleSystem> particles { get { return m_particles; } }
+	#endregion
+	// ********************************************************************
+
+
+}
+// ************************************************************************
+#endregion
+// ************************************************************************
+
+}
