f2a64cd565d25a5d5b57d7a6a01729466c0638e9
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-09-22 21:08:22 +0100

    SpriteGroup - Script for organising a group of sprites to be easily found by other scripts

---
+++ Graphics/SpriteGroup.cs
@@ -0,0 +1,54 @@
+﻿// ************************************************************************ 
+// File Name:   SpriteGroup.cs 
+// Purpose:    	Specify a group of sprites for various purposes
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2017 Bounder Games
+// ************************************************************************
+namespace BounderFramework { 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BounderFramework;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: SpriteGroup
+// ************************************************************************ 
+public class SpriteGroup : MonoBehaviour 
+{
+	// ********************************************************************
+	#region Exposed Data Members 
+	// ********************************************************************
+	[SerializeField]
+	[Tooltip("ID for this")]
+	private string m_id = "";
+	[SerializeField]
+	[Tooltip("List of sprites")]
+	private List<SpriteRenderer> m_sprites = new List<SpriteRenderer>();
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Properties 
+	// ********************************************************************
+	public string id { get { return m_id; } }
+	public List<SpriteRenderer> sprites { get { return m_sprites; } }
+	#endregion
+	// ********************************************************************
+
+
+}
+// ************************************************************************
+#endregion
+// ************************************************************************
+
+}
