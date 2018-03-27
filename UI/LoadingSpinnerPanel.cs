a2c07930d02764df4221a5120c4bcba6529a6ee7
Sarah Herzog <sarahherzog@fife.ac.uk>
2018-03-27 20:47:51 +0100

    LoadingSpinnerPanel and MessagePanel added

---
+++ UI/LoadingSpinnerPanel.cs
@@ -0,0 +1,87 @@
+﻿// ************************************************************************ 
+// File Name:   LoadingSpinnerPanel.cs 
+// Purpose:    	Show a loading spinner and block input
+// Project:		Armorued Engines
+// Author:      Sarah Herzog  
+// Copyright: 	2018 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BounderFramework;
+using UnityEngine.UI;
+using Fiftytwo;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: LoadingSpinnerPanelData
+// ************************************************************************
+[System.Serializable]
+public class LoadingSpinnerPanelData : PanelData 
+{
+	// ********************************************************************
+	#region Public Data Members 
+	// ********************************************************************
+	public string loadingText;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Constructors 
+	// ********************************************************************
+	public LoadingSpinnerPanelData(string _loadingText = "",
+	                         PanelState _startingState = PanelState.HIDDEN,
+	                         PanelLimitOverride _limitOverride = PanelLimitOverride.REPLACE)
+		: base(_startingState, _limitOverride)
+	{
+		loadingText = _loadingText;
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: LoadingSpinnerPanel
+// ************************************************************************
+public class LoadingSpinnerPanel : Panel 
+{
+
+	// ********************************************************************
+	#region Exposed Data Members 
+	// ********************************************************************
+	[SerializeField]
+	private Text m_loadingText;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Panel Methods 
+	// ********************************************************************
+	protected override void _Initialise(PanelData _data) 
+	{
+		LoadingSpinnerPanelData castData = _data as LoadingSpinnerPanelData;
+		if (castData != null)
+		{
+			m_loadingText.text = castData.loadingText;
+		}
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
---
+++ UI/MessagePanel.cs
@@ -0,0 +1,151 @@
+﻿// ************************************************************************ 
+// File Name:   MessagePanel.cs 
+// Purpose:    	Simple pop up with a title, message, and one or two buttons
+// Project:		Armorued Engines
+// Author:      Sarah Herzog  
+// Copyright: 	2018 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BounderFramework;
+using UnityEngine.UI;
+using Fiftytwo;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: MessagePanelData
+// ************************************************************************
+[System.Serializable]
+public class MessagePanelData : PanelData 
+{
+	// ********************************************************************
+	#region Public Data Members 
+	// ********************************************************************
+	public string titleText;
+	public string messageText;
+	public string positiveButtonText;
+	public string negativeButtonText;
+	public MessagePanel.ButtonPressed positiveCallback;
+	public MessagePanel.ButtonPressed negativeCallback;
+	#endregion
+	// ********************************************************************
+}
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: MessagePanel
+// ************************************************************************
+public class MessagePanel : Panel 
+{
+
+	// ********************************************************************
+	#region Exposed Data Members 
+	// ********************************************************************
+	[SerializeField]
+	private Text m_titleText;
+	[SerializeField]
+	private Text m_messageText;
+	[SerializeField]
+	private Text m_positiveButtonText;
+	[SerializeField]
+	private Text m_negativeButtonText;
+	[SerializeField]
+	private GameObject m_positiveButton;
+	[SerializeField]
+	private GameObject m_negativeButton;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Private Data Members 
+	// ********************************************************************
+	public ButtonPressed m_positiveCallback = null;
+	public ButtonPressed m_negativeCallback = null;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Events 
+	// ********************************************************************
+	public delegate void ButtonPressed();
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Panel Methods 
+	// ********************************************************************
+	protected override void _Initialise(PanelData _data) 
+	{
+		MessagePanelData castData = _data as MessagePanelData;
+		if (castData != null)
+		{
+			m_titleText.text = castData.titleText;
+			m_messageText.text = castData.messageText;
+			m_positiveButtonText.text = castData.positiveButtonText;
+			m_negativeButtonText.text = castData.negativeButtonText;
+			m_positiveCallback = castData.positiveCallback;
+			m_negativeCallback = castData.negativeCallback;
+
+			if (m_positiveCallback == null && m_negativeCallback == null)
+			{
+				m_positiveButton.SetActive(true);
+				m_negativeButton.SetActive(false);
+			}
+			else
+			{
+				m_positiveButton.SetActive(m_positiveCallback != null);
+				m_negativeButton.SetActive(m_negativeCallback != null);
+			}
+		}
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Public Methods 
+	// ********************************************************************
+	public void PositivePressed()
+	{
+		if (m_positiveCallback != null)
+		{
+			m_positiveCallback();
+		}	
+		else
+		{
+			Close(); // close this panel
+		}
+	}
+	// ********************************************************************
+	public void NegativePressed()
+	{
+		if (m_negativeCallback != null)
+		{
+			m_negativeCallback();
+		}
+		else
+		{
+			Close(); // close this panel
+		}
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
