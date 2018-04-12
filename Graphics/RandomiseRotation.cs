2cf24ab714a4b8452e12a63fadb27a17b36231a0
Sarah Herzog <sarahherzog@fife.ac.uk>
2018-04-12 10:16:03 +0100

    RandomiseRotation - sets a random rotation between two angles (2D)

---
+++ Graphics/RandomiseRotation.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomiseRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+// ************************************************************************ 
+// File Name:   RandomiseRotation.cs 
+// Purpose:    	Randomise rotation between two angles
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2018 Bounder Games
+// ************************************************************************
+namespace BounderFramework { 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BounderFramework;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: RandomiseRotation
+// ************************************************************************ 
+public class RandomiseRotation : MonoBehaviour 
+{
+	// ********************************************************************
+	#region Exposed Data Members 
+	// ********************************************************************
+	[SerializeField]
+	private float m_lowerAngleBound;
+	[SerializeField]
+	private float m_upperAngleBound;
+	[SerializeField]
+	[Tooltip("Should the random rotation be applied on start")]
+	private bool m_applyOnStart = true;
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Monobehavior Methods 
+	// ********************************************************************
+	void Start () 
+	{
+		if (m_applyOnStart)
+			ApplyRandomRotation();
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+
+	// ********************************************************************
+	#region Public Methods 
+	// ********************************************************************
+	public void ApplyRandomRotation()
+	{
+		float angle = Random.Range(m_lowerAngleBound,m_upperAngleBound);
+		transform.Rotate(0,0,angle);
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+}
+// ************************************************************************
+#endregion
+// ************************************************************************
+
+}
