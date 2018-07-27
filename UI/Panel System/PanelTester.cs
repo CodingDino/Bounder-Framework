bc08c0e36a858e9f73042d08e232cea320a16e13
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-07-27 16:04:15 +0100

    PanelTester - script for opening a panel for testing

---
+++ UI/Panel System/PanelTester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTester : MonoBehaviour {
+
+	public Panel m_panel;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return null;
+		PanelManager.OpenPanel(m_panel);
+	}
+}
