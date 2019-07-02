fcf56886e101674b62602e0e058943f841483296
Sarah Herzog <sarah@boundergames.com>
2019-07-02 17:48:38 +0100

    * Added constraint extensions to allow clearing of all constraint sources at once

---
+++ ExtensionMethods/ConstraintExtension.cs
@@ -0,0 +1,51 @@
+﻿// ************************************************************************ 
+// File Name:   ConstraintExtension.cs 
+// Purpose:    	Extends the various contraint classes
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2019 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+using UnityEngine.Animations;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: ConstraintExtension
+// ************************************************************************
+public static class ConstraintExtension 
+{
+	// ********************************************************************
+	#region Extension Methods 
+	// ********************************************************************
+	public static void ClearSources(this ParentConstraint _constraint)
+	{
+		for (int i = _constraint.sourceCount - 1; i >= 0; --i)
+		{
+			_constraint.RemoveSource(i);
+		}
+	}
+	// ********************************************************************
+	public static void AddSourceTransform(this ParentConstraint _constraint, Transform _transform)
+	{
+		if (_transform != null)
+		{
+			ConstraintSource source = new ConstraintSource();
+			source.sourceTransform = _transform;
+			source.weight = 100;
+			_constraint.AddSource(source);
+		}
+	}
+	// ********************************************************************
+	#endregion
+	// ********************************************************************
+
+}
+#endregion
+// ************************************************************************
