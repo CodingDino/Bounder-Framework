bbb13336fd9019ddd87b9a6c0916bfa572093a6b
Sarah Herzog <sarah@boundergames.com>
2019-07-16 11:31:51 +0100

    - Updated .gitignore to no longer include base Unity ignores as this folder is not at the root of a unity project - Added BuildConfiguration base class to include basic setup all games might need (base player profile, version number and debug menu display)

---
+++ Build/BuildConfiguration.cs
@@ -0,0 +1,45 @@
+﻿// ************************************************************************ 
+// File Name:   BuildConfiguration.cs 
+// Purpose:    	Defines system configurations for the game, for different 
+//              build purposes
+// Project:		Bounder Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2019 Bounder Games
+// ************************************************************************ 
+namespace Bounder.Framework
+{
+    // ************************************************************************ 
+    #region Imports
+    // ************************************************************************
+    using UnityEngine;
+    #endregion
+    // ************************************************************************
+
+
+    // ************************************************************************ 
+    #region Class: BuildConfiguration
+    // ************************************************************************
+    [CreateAssetMenu(fileName = "BuildConfiguration", menuName = "Bounder/Settings/BuildConfiguration", order = 1)]
+    public class BuildConfiguration : SettingsDefinition
+    {
+        // ********************************************************************
+        #region Exposed Data Members
+        // ********************************************************************
+
+        // ********************************************************************
+        [Header("Framework Build Settings")]
+        // ********************************************************************
+        [SerializeField] private PlayerProfile m_baseProfile = null;
+        public PlayerProfile baseProfile { get { return m_baseProfile; } }
+        [SerializeField] private bool m_displayVersionNum = false;
+        public bool displayVersionNum { get { return m_displayVersionNum; } }
+        [SerializeField] private bool m_enableDebugMenu = false;
+        public bool enableDebugMenu { get { return m_enableDebugMenu; } }
+        // ********************************************************************
+        
+        #endregion
+        // ********************************************************************
+    }
+    #endregion
+    // ************************************************************************
+}
