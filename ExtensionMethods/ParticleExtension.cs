5b59876d488b5144f42ccb9b717bf95306a2f741
SarahHerzog <235290+CodingDino@users.noreply.github.com>
2022-12-16 08:30:35 +0000

    Added an extension method to the particle system to allow fade out of all active particles in the system

---
+++ ExtensionMethods/ParticleExtension.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public static class ParticleExtension
+{
+    public static IEnumerator FadeOutAllParticles(this ParticleSystem _self, float _duration)
+    {
+        float fadeTime = 0;
+
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[_self.main.maxParticles];
+        int numParticlesAlive = _self.GetParticles(particles);
+
+        while (fadeTime <= _duration)
+        {
+            float alphaChange = Time.deltaTime / _duration;
+            for (int i = 0; i < numParticlesAlive; i++)
+            {
+                Color c = particles[i].startColor;
+                c.a -= alphaChange;
+                if (c.a < 0) c.a = 0;
+                particles[i].startColor = c;
+            }
+
+            fadeTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+
+}
