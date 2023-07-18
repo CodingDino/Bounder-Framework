3e42c2809e9fdd9024d7309add5938d3a35a45ad
SarahHerzog <235290+CodingDino@users.noreply.github.com>
2023-07-18 22:21:05 +0100

    Added RendererExtension method for setting alpha

---
+++ ExtensionMethods/RendererExtension.cs
@@ -0,0 +1,66 @@
+// ************************************************************************ 
+// File Name:   RendererExtension.cs 
+// Purpose:    	Extends the Renderer class
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2023 Bounder Games
+// ************************************************************************ 
+
+
+// ************************************************************************ 
+#region Imports
+// ************************************************************************
+using UnityEngine;
+#endregion
+// ************************************************************************
+
+
+// ************************************************************************ 
+#region Class: RendererExtension
+// ************************************************************************
+public static class RendererExtension
+{
+    // ********************************************************************
+    #region Extension Methods 
+    // ********************************************************************
+    public static void SetAlpha(this Renderer renderer, float val)
+    {
+        // Set this object's alpha
+        foreach (Material mat in renderer.materials)
+        {
+            if (mat.HasProperty("_Color"))
+            {
+                mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, val);
+            }
+            else if (mat.HasProperty("_TintColor"))
+            {
+                Color col = mat.GetColor("_TintColor");
+                mat.SetColor("_TintColor", new Color(col.r, col.g, col.b, val));
+            }
+        }
+    }
+    // ********************************************************************
+    public static void SetAlphaRecursive(this Renderer renderer, float val)
+    {
+        // NOTE: Will not work for sprites, if needed, write separate function for sprites
+        // Avoid checking if this renderer is a SpriteRenderer to avoid unneeded dynamic cast to slow down execution
+
+        // Set this object's alpha
+        renderer.SetAlpha(val);
+
+        // Set child object alpha
+        if (renderer.transform.childCount > 0)
+        {
+            foreach (Transform child in renderer.transform)
+            {
+                Renderer childRenderer = child.GetComponent<Renderer>();
+                childRenderer?.SetAlphaRecursive(val);
+            }
+        }
+    }
+    // ********************************************************************
+    #endregion
+    // ********************************************************************
+}
+#endregion
+// ************************************************************************
