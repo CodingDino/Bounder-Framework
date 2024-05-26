fc9d91a9ec6fefd9c7e78322d37565a8bdeca905
SarahHerzog <235290+CodingDino@users.noreply.github.com>
2024-05-26 21:06:15 +0100

    BREAKING CHANGE - ButtonPlaySFX now supports many different button actions.

--- UI/Buttons/ButtonPlaySFX.cs
+++ UI/Buttons/ButtonPlaySFX.cs
@@ -12,6 +12,8 @@
 // ************************************************************************
 using UnityEngine;
 using Bounder.Framework;
+using UnityEngine.EventSystems;
+using System;
 #endregion
 // ************************************************************************
 
@@ -19,30 +21,105 @@ using Bounder.Framework;
 // ************************************************************************ 
 #region Class: ButtonPlaySFX
 // ************************************************************************
-public class ButtonPlaySFX : MonoBehaviour 
+public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerClickHandler, ISelectHandler, ISubmitHandler
 {
 
-	// ********************************************************************
-	#region Exposed Data Members 
-	// ********************************************************************
-	[SerializeField]
-	private AudioClip m_SFX = null;
-	#endregion
-	// ********************************************************************
+    [Flags]
+	public enum EffectType
+    {
+        NONE = 0,
+        // ---
+        HIGHLIGHT = 1 << 0,
+        SELECT = 1 << 1,
+        PRESS_DOWN = 1 << 2,
+        CLICK = 1 << 3,
+        SUBMIT = 1 << 4,
+        // ---
+        HIGHLIGHT_OR_SELECT = SELECT | HIGHLIGHT,
+        CLICK_OR_SUBMIT = CLICK | SUBMIT
+    }
 
-
-	// ********************************************************************
-	#region Public Methods 
-	// ********************************************************************
-	public void PlayClick () 
+	[System.Serializable]
+	public class ButtonSFXInfo
 	{
-		AudioObject audio = AudioManager.Play(m_SFX,AudioCategory.EFFECTS);
-		if (audio != null)
-			audio.audioSource.ignoreListenerPause = true;
+		public EffectType type = EffectType.NONE;
+		public AudioInfo info = new AudioInfo();
 	}
-	// ********************************************************************
-	#endregion
-	// ********************************************************************
+
+
+    // ********************************************************************
+    #region Exposed Data Members 
+    // ********************************************************************
+    [SerializeField]
+    private ButtonSFXInfo[] buttonSFXInfo;
+    #endregion
+    // ********************************************************************
+
+
+    // ********************************************************************
+    #region Public Methods 
+    // ********************************************************************
+    public void OnPointerEnter(PointerEventData ped)
+    {
+        Debug.Log("OnPointerEnter");
+        for (int i = 0; i < buttonSFXInfo.Length; ++i)
+        {
+            if (buttonSFXInfo[i].type.Contains(EffectType.HIGHLIGHT))
+            {
+                AudioManager.Play(buttonSFXInfo[i].info);
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        Debug.Log("OnPointerDown");
+        for (int i = 0; i < buttonSFXInfo.Length; ++i)
+        {
+            if (buttonSFXInfo[i].type.Contains(EffectType.PRESS_DOWN))
+            {
+                AudioManager.Play(buttonSFXInfo[i].info);
+            }
+        }
+    }
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.Log("OnPointerClick");
+        for (int i = 0; i < buttonSFXInfo.Length; ++i)
+        {
+            if (buttonSFXInfo[i].type.Contains(EffectType.CLICK))
+            {
+                AudioManager.Play(buttonSFXInfo[i].info);
+            }
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("OnSelect");
+        for (int i = 0; i < buttonSFXInfo.Length; ++i)
+        {
+            if (buttonSFXInfo[i].type.Contains(EffectType.SELECT) && InputManager.useDirectionalUINavigation)
+            {
+                AudioManager.Play(buttonSFXInfo[i].info);
+            }
+        }
+    }
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        Debug.Log("OnSubmit");
+        for (int i = 0; i < buttonSFXInfo.Length; ++i)
+        {
+            if (buttonSFXInfo[i].type.Contains(EffectType.SUBMIT))
+            {
+                AudioManager.Play(buttonSFXInfo[i].info);
+            }
+        }
+    }
+    // ********************************************************************
+    #endregion
+    // ********************************************************************
 
 }
 #endregion
