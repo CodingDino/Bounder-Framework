c9265d74520219b72f976a23f63c2d4a39bd616b
SarahHerzog <235290+CodingDino@users.noreply.github.com>
2024-06-13 22:02:14 +0100

    FMod Integration of audio system

---
+++ Audio/AudioManagerFM.cs
@@ -0,0 +1,160 @@
+// ************************************************************************ 
+// File Name:   AudioInfoFM.cs 
+// Purpose:    	Information on a piece of audio, using FMOD
+// Project:		Framework
+// Author:      Sarah Herzog  
+// Copyright: 	2024 Bounder Games
+// ************************************************************************
+namespace Bounder.Framework
+{
+
+    // ************************************************************************ 
+    #region Imports
+    // ************************************************************************
+    using FMODUnity;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    #endregion
+    // ************************************************************************
+
+
+    // ************************************************************************ 
+    #region Class: AudioInfoFM
+    // ************************************************************************
+    public class AudioManagerFM : Singleton<AudioManagerFM>
+    {
+        // ********************************************************************
+        #region Exposed Data Members
+        // ********************************************************************
+        [SerializeField]
+        private StudioEventEmitter m_emitterPrefab = null;
+        [SerializeField]
+        private float m_musicSwapWaitTime = 1.0f;
+        #endregion
+        // ********************************************************************
+
+
+        // ********************************************************************
+        #region Exposed Data Members
+        // ********************************************************************
+        private Dictionary<EventReference, ObjectPool> m_objectPools = new();
+        private StudioEventEmitter m_currentMusic = null;
+        private AudioInfoFM m_targetMusic = null;
+        private Coroutine m_swapMusicCR = null;
+        #endregion
+        // ********************************************************************
+
+
+        // ********************************************************************
+        #region Public Methods
+        // ********************************************************************
+        public static void PlayAsOneShot(AudioInfoFM audioInfo, Transform parent = null)
+        {
+            if (parent == null)
+                RuntimeManager.PlayOneShot(audioInfo.eventRef);
+            else
+                RuntimeManager.PlayOneShotAttached(audioInfo.eventRef, parent.gameObject);
+        }
+        // ********************************************************************
+        public static void PlayAsOneShotAtLocation(AudioInfoFM audioInfo, Vector3 location)
+        {
+            RuntimeManager.PlayOneShot(audioInfo.eventRef, location);
+        }
+        // ********************************************************************
+        public static StudioEventEmitter CreateAndPlayEmitter(AudioInfoFM audioInfo, Transform parent = null)
+        {
+            if (!instance.m_objectPools.ContainsKey(audioInfo.eventRef))
+            {
+                instance.m_objectPools[audioInfo.eventRef] = new ObjectPool(instance.m_emitterPrefab.gameObject);
+            }
+
+            GameObject spawnedObject = instance.m_objectPools[audioInfo.eventRef].RequestObject(parent);
+            spawnedObject.transform.localPosition = Vector3.zero;
+
+            StudioEventEmitter emitter = spawnedObject.GetComponent<StudioEventEmitter>();
+            emitter.EventReference = audioInfo.eventRef;
+            emitter.Play();
+
+            return emitter;
+        }
+        // ********************************************************************
+        public static StudioEventEmitter CreateAndPlayEmitterAtLocation(AudioInfoFM audioInfo, Vector3 location, Transform parent = null)
+        {
+            StudioEventEmitter emitter = CreateAndPlayEmitter(audioInfo, parent);
+            emitter.transform.position = location;
+            return emitter;
+        }
+        // ********************************************************************
+        public static void PlayAsMusic(AudioInfoFM audioInfo)
+        {
+            
+            instance.m_targetMusic = audioInfo;
+
+            if (instance.m_swapMusicCR == null)
+            {
+                instance.m_swapMusicCR = instance.StartCoroutine(instance.SwapMusicCR());
+            }
+        }
+        // ********************************************************************
+        public static StudioEventEmitter GetMusic()
+        {
+            return instance.m_currentMusic;
+        }
+        // ********************************************************************
+        public static void PauseMusic()
+        {
+            if (instance.m_currentMusic != null)
+                instance.m_currentMusic.EventInstance.setPaused(true);
+        }
+        // ********************************************************************
+        public static void ResumeMusic()
+        {
+            if (instance.m_currentMusic != null)
+                instance.m_currentMusic.EventInstance.setPaused(false);
+        }
+        // ********************************************************************
+        public static void StopMusic()
+        {
+            if (instance.m_currentMusic != null)
+                instance.m_currentMusic.Stop();
+        }
+        #endregion
+        // ********************************************************************
+
+
+        // ********************************************************************
+        #region Private Methods
+        // ********************************************************************
+        private IEnumerator SwapMusicCR()
+        {
+            // Fade out existing music
+            if (m_currentMusic != null)
+            {
+                m_currentMusic.Stop();
+
+                // Wait for music to fade out
+                yield return new WaitForSeconds(m_musicSwapWaitTime);
+
+                m_currentMusic.gameObject.SetActive(false);
+            }
+
+            // New music
+            m_currentMusic = CreateAndPlayEmitter(m_targetMusic);
+
+            // All done, clear coroutine and cleanup target music
+            m_swapMusicCR = null;
+            m_targetMusic = null;
+
+            yield break;
+        }
+        #endregion
+        // ********************************************************************
+
+
+    }
+    #endregion
+    // ************************************************************************
+
+
+}
--- UI/Buttons/ButtonPlaySFX.cs
+++ UI/Buttons/ButtonPlaySFX.cs
@@ -45,7 +45,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
 	public class ButtonSFXInfo
 	{
 		public EffectType type = EffectType.NONE;
-		public AudioInfo info = new AudioInfo();
+		public AudioInfoFM info = new AudioInfoFM();
 	}
 
 
@@ -85,7 +85,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
             {
                 if (buttonSFXInfo[i].type.Contains(EffectType.HIGHLIGHT))
                 {
-                    AudioManager.Play(buttonSFXInfo[i].info);
+                    AudioManagerFM.PlayAsOneShot(buttonSFXInfo[i].info);
                 }
             }
         }
@@ -100,7 +100,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
             {
                 if (buttonSFXInfo[i].type.Contains(EffectType.PRESS_DOWN))
                 {
-                    AudioManager.Play(buttonSFXInfo[i].info);
+                    AudioManagerFM.PlayAsOneShot(buttonSFXInfo[i].info);
                 }
             }
         }
@@ -115,7 +115,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
             {
                 if (buttonSFXInfo[i].type.Contains(EffectType.CLICK))
                 {
-                    AudioManager.Play(buttonSFXInfo[i].info);
+                    AudioManagerFM.PlayAsOneShot(buttonSFXInfo[i].info);
                 }
             }
         }
@@ -130,7 +130,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
             {
                 if (buttonSFXInfo[i].type.Contains(EffectType.SELECT) && InputManager.useDirectionalUINavigation)
                 {
-                    AudioManager.Play(buttonSFXInfo[i].info);
+                    AudioManagerFM.PlayAsOneShot(buttonSFXInfo[i].info);
                 }
             }
         }
@@ -145,7 +145,7 @@ public class ButtonPlaySFX : MonoBehaviour, IPointerEnterHandler, IPointerDownHa
             {
                 if (buttonSFXInfo[i].type.Contains(EffectType.SUBMIT))
                 {
-                    AudioManager.Play(buttonSFXInfo[i].info);
+                    AudioManagerFM.PlayAsOneShot(buttonSFXInfo[i].info);
                 }
             }
         }
